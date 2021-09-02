a530f2f8b047c6830acde39cf4b3f66cc620ebd8
Jared Whaley <jjw0008@auburn.edu>
2021-09-02 17:02:41 -0400

    Moved all word occurence calculation to the "WordCalculator" class and implemented those changes within the form.

--- WordOccurenceCalculator/Form1.cs
+++ WordOccurenceCalculator/Form1.cs
@@ -13,9 +13,6 @@ namespace WordOccurenceCalculator
 {
     public partial class Form1 : Form
     {
-        List<string> wordsList = new List<string>();
-        List<WordOccurrence> woList = new List<WordOccurrence>();
-        Dictionary<string, int> woDict = new Dictionary<string, int>();
 
         public Form1()
         {
@@ -24,33 +21,14 @@ namespace WordOccurenceCalculator
 
         private void calculateButton_Click(object sender, EventArgs e)
         {
+            List<string> wordsList = new List<string>();
             wordsList = Regex.Split(inputTextBox.Text, "[^a-zA-Z']+").ToList<string>();
+            List<WordOccurrence> woList = new List<WordOccurrence>();
+
             string outputString = "";
-            for (int i = 0; i < wordsList.Count; i++)
-            {
-                wordsList[i] = wordsList[i].ToLower();
-                wordsList[i] = char.ToUpper(wordsList[i][0]) + wordsList[i].Substring(1);
-                try
-                {
-                    woDict.Add(wordsList[i], 1);
-                    woList.Add(new WordOccurrence(wordsList[i]));
-                }
-                catch (ArgumentException)
-                {
-                    woDict[wordsList[i]] += 1;
-                    for (int j = 0; j < woList.Count; j++)
-                    {
-                        if (woList[j].Word.Equals(wordsList[i]))
-                        {
-                            woList[j].Count += 1;
-                            break;
-                        }
-                    }
-                }
-            }
+            woList = WordCalculator.CalculateOccurrences(wordsList);
             for (int i = 0; i < woList.Count; i++)
             {
-                outputString += ("DICT: " + woDict.Keys.ElementAt(i) + "\t" + woDict.Values.ElementAt(i).ToString() + "\n");
                 outputString += ("WOCC: " + woList[i].Word + "\t" + woList[i].Count.ToString() + "\n\n");
             }
                 MessageBox.Show(outputString);
--- WordOccurenceCalculator/WordCalculator.cs
+++ WordOccurenceCalculator/WordCalculator.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace WordOccurenceCalculator
 {
+
     class WordCalculator
     {
         /**
@@ -13,15 +14,41 @@ namespace WordOccurenceCalculator
          * 
          * </summary>
          */
-        static List<WordOccurrence> CalculateOccurrences(List<string> occurences)
+        public static List<WordOccurrence> CalculateOccurrences(List<string> wordsList)
         {
             // If the input list is uninitialized or empty, throw an exception.
-            if (occurences == null || occurences.Any())
+            if (wordsList == null || !wordsList.Any())
             {
                 throw new ArgumentException("Invalid input.");
             }
 
-            return null; //TODO
+            List<WordOccurrence> woList = new List<WordOccurrence>();
+            Dictionary<string, byte> woDict = new Dictionary<string, byte>();
+
+            for (int i = 0; i < wordsList.Count; i++)
+            {
+                // Convert all strings to lowercase and then capitalize the first letter to avoid erroneous duplicates based on casing.
+                wordsList[i] = wordsList[i].ToLower();
+                wordsList[i] = char.ToUpper(wordsList[i][0]) + wordsList[i].Substring(1);
+                try
+                {
+                    woDict.Add(wordsList[i], 0);
+                    woList.Add(new WordOccurrence(wordsList[i]));
+                }
+                catch (ArgumentException)
+                {
+                    for (int j = 0; j < woList.Count; j++)
+                    {
+                        if (woList[j].Word.Equals(wordsList[i]))
+                        {
+                            woList[j].Count += 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return woList;
         }
     }
 }
