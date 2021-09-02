e242f76a11af555dfab51afff93eb821ddb920e1
Jared Whaley <jjw0008@auburn.edu>
2021-09-02 16:01:21 -0400

    Added functionality to the "Calculate" button; now splits the text from our input field and creates a string list using regex. Temporarily implemented a MessageBox for output testing.

--- WordOccurenceCalculator/Form1.cs
+++ WordOccurenceCalculator/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,18 @@ namespace WordOccurenceCalculator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            List<string> wordsList = new List<string>();
+            wordsList = Regex.Split(inputTextBox.Text, "[^a-zA-Z']+").ToList<string>();
+            string outputString = "";
+            for (int i = 0; i < wordsList.Count; i++)
+            {
+                outputString += (wordsList[i] + "\n");
+            }
+            MessageBox.Show(outputString);
         }
     }
 }
