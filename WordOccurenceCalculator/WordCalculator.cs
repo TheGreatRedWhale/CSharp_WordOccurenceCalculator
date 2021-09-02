081d805508c83ecb7fa5f08ca8910b352a0473e4
Jared Whaley <jjw0008@auburn.edu>
2021-09-02 15:17:26 -0400

    Added "throw" clause to "CalculateOccurences" method.

--- WordOccurenceCalculator/WordCalculator.cs
+++ WordOccurenceCalculator/WordCalculator.cs
@@ -8,8 +8,19 @@ namespace WordOccurenceCalculator
 {
     class WordCalculator
     {
+        /**
+         * <summary>
+         * 
+         * </summary>
+         */
         static List<WordOccurrence> CalculateOccurrences(List<string> occurences)
         {
+            // If the input list is uninitialized or empty, throw an exception.
+            if (occurences == null || occurences.Any())
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+
             return null; //TODO
         }
     }
