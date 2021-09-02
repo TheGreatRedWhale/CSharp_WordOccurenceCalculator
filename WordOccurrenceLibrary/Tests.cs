435fdb5f436cea82baa7fd53c804ac3466ac4b0e
Jared Whaley <jjw0008@auburn.edu>
2021-09-02 19:02:23 -0400

    Realized that I had created a class library dependant on the wrong package. Removed that version and created a new test library dependant on the correct packages. Also added NUnit testing.

---
+++ WordOccurrenceLibrary/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordOccurrenceLibrary
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void CalculateOccurrencesTest()
+        {
+            List<string> input = new List<string>();
+            string expected = "This: 1Isn't: 1Very: 1Hard: 1";
+            string actual = "";
+            input.Add("this");
+            input.Add("ISn'T");
+            input.Add("VeRy");
+            input.Add("hARD");
+            for (int i = 0; i < input.Count; i++)
+            {
+                actual += input[i];
+            }
+            Assert.IsTrue(expected.Equals(actual));
+        }
+    }
+}
